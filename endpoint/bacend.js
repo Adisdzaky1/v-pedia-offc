89f67a3f2af9af0a7715210826244290e2aa647e
X-Rezz <162150873+RerezzOfficial@users.noreply.github.com>
2025-07-12 19:11:03 +0700

    Update bacend.js

--- endpoint/bacend.js
+++ endpoint/bacend.js
@@ -593,17 +593,22 @@ router.get("/produk-provider", requireLogin, async (req, res) => {
       type: "prabayar",
       code: "",
     };
-const urlnya = `${BASE_URL}/layanan/price_list`
+
+    const urlnya = `${BASE_URL}/layanan/price_list`;
+
     const atlanticResponse = await axios.post(
       urlnya,
       qs.stringify(formDataToAtlantic),
       {
-        headers: { 
-          "Content-Type": "application/x-www-form-urlencoded",
-          'User-Agent': 'MyCustomUserAgent/1.0 (compatible; RerezzBot/2025)',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Referer': urlnya,
-        },
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          'Accept-Language': 'en-US,en;q=0.9',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Origin': 'https://v-pedia.web.id',
+          'Referer': urlnya,
+          'Connection': 'keep-alive'
+        }
       }
     );
 
